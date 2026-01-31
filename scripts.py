2cbeb8d0c257d52c1743ba9b551bccde9fc62288
ravindra00123 <ravindra.ku9332@gmail.com>
2026-01-30 19:36:35 -0500

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-# CSV file ko load karein
-df = pd.read_csv('data.csv')
+# Sahi 'Raw' URL use karein
+url = 'https://raw.githubusercontent.com/ravindra00123/demo2/main/sales_data.csv'
+
+# CSV file load karein
+df = pd.read_csv(url)
 
 # Example kaam: Har row mein ek column add karein
 df['New_Column'] = 'Processed'
 
 # Naya result save karein
 df.to_csv('result.csv', index=False)
+
 print("File processed successfully!")
